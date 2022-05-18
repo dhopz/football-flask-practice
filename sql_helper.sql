4656944632ca7aa533c4d654e6cb6c1eb50b8fe7
dhopz <david_houpapa@hotmail.com>
2022-05-18 16:05:50 +0100

    table structure complete

--- sql_helper.sql
+++ sql_helper.sql
@@ -83,7 +83,9 @@ FROM(
     WHERE league_id = 1729 AND season = '2008/2009') as m2
 WHERE awayteam_id = 9825;
 
-SELECT COUNT(result) AS games_played,
+-- table structure
+SELECT team_long_name AS team,
+COUNT(result) AS games_played,
 SUM(CASE WHEN result = 'W' THEN 1 ELSE 0 END) as games_won,
 SUM(CASE WHEN result = 'D' THEN 1 ELSE 0 END) as games_drawn,
 SUM(CASE WHEN result = 'L' THEN 1 ELSE 0 END) as games_lost,
@@ -96,6 +98,31 @@ SUM(
         WHEN result = 'D' THEN 1
         ELSE 0 END
 ) as points
-FROM arsenal_results;
+FROM match_results
+LEFT JOIN team
+ON match_results.team_id = team.team_api_id
+GROUP BY team_long_name
+ORDER BY points DESC, goal_difference DESC;
 
+-- Mtch Results
+CREATE OR REPLACE VIEW match_results AS
+SELECT hometeam_id AS team_id,home_goal AS goals_for,away_goal AS goals_against,
+CASE 
+    WHEN home_goal > away_goal THEN 'W'
+    WHEN home_goal = away_goal THEN 'D'
+    ELSE 'L' END AS result
+FROM(
+    SELECT date, season,hometeam_id,awayteam_id,home_goal,away_goal
+    FROM match
+    WHERE league_id = 1729 AND season = '2008/2009') as m1
+UNION ALL
+SELECT awayteam_id AS team_id,away_goal AS goals_for,home_goal AS goals_against,
+CASE 
+    WHEN home_goal < away_goal THEN 'W'
+    WHEN home_goal = away_goal THEN 'D'
+    ELSE 'L' END AS result
+FROM(
+    SELECT date, season,hometeam_id,awayteam_id,home_goal,away_goal
+    FROM match
+    WHERE league_id = 1729 AND season = '2008/2009') as m2;
 
