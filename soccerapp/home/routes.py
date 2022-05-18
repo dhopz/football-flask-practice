eff8e3a2c648837cdac0e8d86c62ee037b6c5b64
dhopz <david_houpapa@hotmail.com>
2022-05-18 16:14:16 +0100

    team standings for premier league created

--- soccerapp/home/routes.py
+++ soccerapp/home/routes.py
@@ -40,3 +40,34 @@ def teams():
 		{"team":team.team_long_name} for team in teams
 	]
 	return {"teams": results, "message":"success"}
+
+@home_bp.route('/league_table', methods=['GET'])
+def league_table():
+	league_table = session.execute(
+		f"SELECT team_long_name AS team,\n"
+		f"COUNT(result) AS games_played,\n"
+		f"SUM(CASE WHEN result = 'W' THEN 1 ELSE 0 END) as games_won,\n"
+		f"SUM(CASE WHEN result = 'D' THEN 1 ELSE 0 END) as games_drawn,\n"
+		f"SUM(CASE WHEN result = 'L' THEN 1 ELSE 0 END) as games_lost,\n"
+		f"SUM(goals_for) AS goals_for,\n" 
+		f"SUM(goals_against) as goals_against,\n"
+		f"SUM(goals_for)-SUM(goals_against) AS goal_difference,\n"
+		f"SUM(CASE WHEN result = 'W' THEN 3 WHEN result = 'D' THEN 1 ELSE 0 END) as points\n"
+		f"FROM match_results\n"
+		f"LEFT JOIN team\n"
+		f"ON match_results.team_id = team.team_api_id\n"
+		f"GROUP BY team_long_name\n"
+		f"ORDER BY points DESC, goal_difference DESC;"
+	)
+	results = [
+		{"team":team.team,
+		"games_played":team.games_played,
+		"games_won":team.games_won,
+		"games_drawn":team.games_drawn,
+		"games_lost":team.games_lost,
+		"goals_for":team.goals_for,
+		"goals_against":team.goals_against,
+		"goal_difference":team.goal_difference,
+		"points":team.points
+		} for team in league_table]
+	return {"teams": results, "message":"success"}
