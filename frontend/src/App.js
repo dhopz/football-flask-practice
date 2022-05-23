7541174ed4612fa852cdaf72171a1dfa01226db3
dhopz <david_houpapa@hotmail.com>
2022-05-23 17:24:18 +0100

    table created for frontend teams

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
+import React, { Fragment, useState, useEffect } from 'react';
+import { DataGrid } from '@mui/x-data-grid'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    useEffect(() => {
+        const getAPI = () => {
+            // Change this endpoint to whatever local or online address you have
+            // Local PostgreSQL Database
+            const API = 'http://127.0.0.1:5000/teams';
+
+            
+            fetch(API)
+                .then((response) => {
+                    console.log(response);
+                    return response.json();
+                })
+                .then((data) => {
+                    console.log(data);
+                    setLoading(false);
+                    setApiData(data);
+                });
+        };
+        getAPI();
+    }, []);
+    const [apiData, setApiData] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    const columns = [
+      { field: 'id', headerName: 'ID', width: 70 },
+      { field: 'firstName', headerName: 'First name', width: 130 },
+      { field: 'lastName', headerName: 'Last name', width: 130 },
+      {
+        field: 'age',
+        headerName: 'Age',
+        type: 'number',
+        width: 90,
+      },
+      {
+        field: 'fullName',
+        headerName: 'Full name',
+        description: 'This column has a value getter and is not sortable.',
+        sortable: false,
+        width: 160,
+        valueGetter: (params) =>
+          `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+      },
+    ];
+    
+    const rows = [
+      { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+      { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+      { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+      { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+      { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+      { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+      { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+      { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+      { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    ];
+    
+    
+      return (
+        
+        <div style={{ height: 400, width: '100%' }}>
+          <Fragment>
+            <header>
+                <h1>Football Data</h1>
+            </header>
+        </Fragment>
+          <DataGrid
+            rows={rows}
+            columns={columns}
+            pageSize={5}
+            rowsPerPageOptions={[5]}
+            checkboxSelection
+          />
+        </div>
+      );
+    }
 
 export default App;
---
+++ frontend/src/Pages/Homepage.js
@@ -0,0 +1,39 @@
+import React, { Fragment, useState, useEffect } from 'react';
+import './App.css';
+
+const App = () => {
+    useEffect(() => {
+        const getAPI = () => {
+            // Change this endpoint to whatever local or online address you have
+            // Local PostgreSQL Database
+            const API = 'http://127.0.0.1:5000/';
+
+            
+            fetch(API)
+                .then((response) => {
+                    console.log(response);
+                    return response.json();
+                })
+                .then((data) => {
+                    console.log(data);
+                    setLoading(false);
+                    setApiData(data);
+                });
+        };
+        getAPI();
+    }, []);
+    const [apiData, setApiData] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    console.log(apiData)
+
+    return (
+        <Fragment>
+            <header>
+                <h1>Football Data</h1>
+            </header>
+        </Fragment>
+    );
+};
+
+export default App;
